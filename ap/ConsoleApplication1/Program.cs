53a3d8c13c5aba20e9ddf2adac137b4d599ed86d
humbertoesp <humberto.esparza193@tectijuana.edu.mx>
2019-10-10 09:10:32 -0700

    mi primer push de repo local a la repo nube nocompilaayudanmeplsmboamorir

---
+++ ap/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine  ("i am the hackerman");
+            Console.ReadKey();
+        }
+    }
+}
